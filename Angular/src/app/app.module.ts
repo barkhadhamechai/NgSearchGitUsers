41246723d44900d27a945fddb825a93d17f6de29
barkhad <barkha@aadhaarapi.com>
2018-06-24 12:06:38 +0530

    created three components user-list, search, sort-by

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { SearchComponent } from './search/search.component';
+import { SortByComponent } from './sort-by/sort-by.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    SearchComponent,
+    SortByComponent
   ],
   imports: [
     BrowserModule
