13eb953b63817697418664fe834231e9b4d4dd9b
barkhad <barkha@aadhaarapi.com>
2018-06-25 09:54:38 +0530

    binded repo details

--- Angular/src/app/user-list/user.model.ts
+++ Angular/src/app/user-list/user.model.ts
@@ -3,6 +3,7 @@ import { RepoDetails } from './repo-details.model';
 export class user implements Deserializable {
 
   repoList:RepoDetails[]=new Array<RepoDetails>();
+  showDetails:boolean=false;
 
   deserialize(input: any) {
     Object.assign(this, input);
